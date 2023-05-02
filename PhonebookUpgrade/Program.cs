5febd5a325ca45c9acaf595ffa1efd30e437a772
Cral_Cactus <marian.pb@abv.bg>
2023-05-02 14:46:48 +0300

    Creation of upgraded Phonebook

---
+++ PhonebookUpgrade/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhoneBookUpgrade
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            var phonebook = new SortedDictionary<string, string>();
+
+            while (input[0] != "END")
+            {
+                if (input[0] == "A")
+                {
+                    phonebook[input[1]] = input[2];
+
+                }
+                else if (input[0] == "S")
+                {
+                    if (phonebook.ContainsKey(input[1]))
+                    {
+                        Console.WriteLine($"{input[1]} -> {phonebook[input[1]]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {input[1]} does not exist.");
+                    }
+                }
+                else if (input[0] == "ListAll")
+                {
+                    foreach (var number in phonebook)
+                    {
+                        Console.WriteLine($"{number.Key} -> {number.Value}");
+                    }
+                }
+
+                input = Console.ReadLine().Split(' ');
+            }
+        }
+    }
+}
