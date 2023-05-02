14576ba5a30cbf4ccb3c1b30e02ba6d8e762ab0a
Cral_Cactus <marian.pb@abv.bg>
2023-05-02 14:47:22 +0300

    Creation  of CountRealNumbers

---
+++ CountRealNums/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountRealNums
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+
+            foreach (string number in numbers)
+            {
+                double num = double.Parse(number);
+
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+            }
+
+            foreach (var pair in counts)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
